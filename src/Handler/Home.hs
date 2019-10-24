d7322046e9aebf52e0d9ec6c58f3bbc2137bbd86
Elder <elder_louzada@outlook.com>
2019-10-24 14:01:32 -0300

    update Home.hs

--- src/Handler/Home.hs
+++ src/Handler/Home.hs
@@ -1,7 +1,7 @@
 {-# LANGUAGE NoImplicitPrelude #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE TemplateHaskell #-}
-{-# LANGUAGE QuaseQuotes #-}
+{-# LANGUAGE QuasiQuotes #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE TypeFamilies #-}
 module Handler.Home where
