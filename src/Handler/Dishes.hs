8c79de638338f297ba24f575c609248b920cedcd
Ubuntu <ubuntu@ip-172-31-87-80.ec2.internal>
2019-11-16 14:20:21 +0000

    add Dishes handler

---
+++ src/Handler/Dishes.hs
@@ -0,0 +1,18 @@
+{-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE QuasiQuotes #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE TypeFamilies #-}
+module Handler.Dishes where
+
+import Import
+-- import Network.HTTP.Types.Status
+import Database.Persist.Postgresql
+import Text.Lucius
+import Text.Julius
+
+getDishesR :: Handler Html
+getDishesR = do 
+    defaultLayout $ do
+        $(widgetFile "/dishes/dishes")
