9655b072d52a5346f4d71beeb6cc2ad3bbaa0af4
reedlex98 <elder_louzada@outlook.com>
2019-12-04 14:53:22 -0300

    -m

--- src/Handler/NewDish.hs
+++ src/Handler/NewDish.hs
@@ -4,7 +4,7 @@
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE QuasiQuotes #-}
 {-# LANGUAGE TypeFamilies #-}
-module Handler.Produto where
+module Handler.NewDish where
 
 import Import
 --import Network.HTTP.Types.Status
