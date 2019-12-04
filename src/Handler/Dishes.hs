df5619c88cc8c7c237056dddfb08d217edd93581
reedlex98 <elder_louzada@outlook.com>
2019-12-04 11:37:13 -0300

    -m

--- src/Handler/Dishes.hs
+++ src/Handler/Dishes.hs
@@ -22,36 +22,36 @@ getDishesR = do
         toWidgetHead $(luciusFile "templates/headerSearchBar/headerSB.lucius")
         [whamlet|
             <nav class="navigation">
-            <div class="logo">
-                <strong>
-                    Cook Time!
-            <div class="search-form">
-                <input class="search-input" name="search-recipe" type="text" placeholder="Encontre uma receita...">
-                <button class="search-button">
-                    <i class="fa fa-search">
-            <ul class="nav-buttons">
-                $maybe nome <- sess
-                    <li>
-                        <a href="">
-                            <i class="fa fa-book">
-                            Envie sua receita
-                    <li>
-                        <a href=@{SairR}>
-                            <i class="fa fa-sign-out">
-                            Sair
-                $nothing 
-                    <li>
-                        <a href=@{UsuarioR}>
-                            <i class="fa fa-book">
-                            Envie sua receita
-                    <li>
-                        <a href=@{UsuarioR}>
-                            <i class="fa fa-user-plus">
-                            Cadastre-se
-                    <li>
-                        <a href=@{EntrarR}>
-                            <i class="fa fa-sign-in">
-                            Entrar
+                <div class="logo">
+                    <strong>
+                        Cook Time!
+                <div class="search-form">
+                    <input class="search-input" name="search-recipe" type="text" placeholder="Encontre uma receita...">
+                    <button class="search-button">
+                        <i class="fa fa-search">
+                <ul class="nav-buttons">
+                    $maybe nome <- sess
+                        <li>
+                            <a href="">
+                                <i class="fa fa-book">
+                                Envie sua receita
+                        <li>
+                            <a href=@{SairR}>
+                                <i class="fa fa-sign-out">
+                                Sair
+                    $nothing 
+                        <li>
+                            <a href=@{UsuarioR}>
+                                <i class="fa fa-book">
+                                Envie sua receita
+                        <li>
+                            <a href=@{UsuarioR}>
+                                <i class="fa fa-user-plus">
+                                Cadastre-se
+                        <li>
+                            <a href=@{EntrarR}>
+                                <i class="fa fa-sign-in">
+                                Entrar
         |]
         $(widgetFile "/categoriesBar/categories")
         $(widgetFile "/dishes/dishes")
